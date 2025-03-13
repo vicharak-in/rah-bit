a8313f8b1fb7a304d4dbabd6a104fbcd6d639a9c
tej-dabhankar <tejasdabhankar123@gmail.com>
2025-03-13 18:45:39 +0530

    example: calc_ops: src added necessary commit and print statement in calc.c
    Update file to make it more interactive for the user by adding necessary comments
    and print statement.

--- examples/calc_ops/src/calc.c
+++ examples/calc_ops/src/calc.c
@@ -16,6 +16,8 @@ void xprintf(char *data, int len) {
 }
 
 int main(int argc, char *argv[]) {
+	/* The example takes 2 inputs of packed 6 bytes and compute the ADD,SHIFT,MULT operations for it, { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 } and { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 } for ADD { 0x02, 0x04, 0x06, 0x08, 0x0a, 0x0c } */	
+	
 	if (argc <= 3) {
 		printf("Usage: adder <num1> <num2> ...\n");
 		exit(EXIT_FAILURE);
@@ -31,16 +33,23 @@ int main(int argc, char *argv[]) {
 	rah_write(MUL, message, sizeof(message));
 
 	char add_result[6];
+	
 	rah_read(ADD, add_result, sizeof(add_result));
+	printf("ADD : ");
 	xprintf(add_result, sizeof(add_result));
+	printf("\n");
 
 	char div_result[6];
 	rah_read(SHIFT, div_result, sizeof(div_result));
+	printf("SHIFT : ");
 	xprintf(div_result, sizeof(div_result));
+	printf("\n");
 
 	char mul_result[12];
 	rah_read(MUL, mul_result, sizeof(mul_result));
+	printf("MULT : ");
 	xprintf(mul_result, sizeof(mul_result));
+	printf("\n");
 
 	exit(EXIT_SUCCESS);
 }
