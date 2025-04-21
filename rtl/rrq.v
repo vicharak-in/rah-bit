36afd79fe52744bce28a3761471b558a23564882
djkabutar <d.kabutarwala@yahoo.com>
2025-04-21 16:57:17 +0530

    rtl: rrq: instead of using occupants for the count use busy signal
    Busy signal will be passed by the encoder. It is the status
    of the rah encoder if it is in use or not.
    
    Signed-off-by: djkabutar <d.kabutarwala@yahoo.com>

--- rtl/rrq.v
+++ rtl/rrq.v
@@ -2,8 +2,7 @@ module rrq (
     input clk,
     input [TOTAL_APPS-1:0] data_queue_empty,
     input read_done,
-    input [TOTAL_APPS-1:0] fifo_re,
-    input [FIFO_ADD_WIDTH-1:0] occupants,
+    input is_busy,
 
     output reg read_queue = 0,
     output reg [APP_ID_WIDTH-1:0] app_id = 0 
@@ -21,51 +20,44 @@ module rrq (
 `pragma protect key_method="rsa"
 `pragma protect encoding=(enctype="base64", line_length=64, bytes=256)
 `pragma protect key_block
-Qg44AZl/GhuBMFe2/NhOzkf6gItZKSsosqY0J5qb/kJTCWhItvA5vhTNdnAU7iGj
-iNjfmfqVrpaLSURxCgrqNWmlenHYgWQvviPBFXPKX03GAQXF0azRqXRAX7eMUR8u
-ZTTsk+29x1yyRJKfqglCaiTEcDeZwcZnstqoWO9t6m2X40cF5/+hIQO6XLa0fbBS
-912PsBBPUHv217U2F8XgqyXMYveunNLfz25fP2tjrahN9yls+MI8Qd1x2movIwHp
-c8oGpY7OUhVhLZdqI/HpdGBPpVHwlHo41S8j3Cc0ZmAHxAnvZ8lnqjvJiIr38FrU
-hhI5/+gWqj87X+/Zj3uvVg==
+OraaImAjJTcHlLCdPotJcQo3TWYQnGaiADhmaot7AiAwCX1C4qszOq+Kzofde/nk
+PskJ5MYn+xpXwSUxc7JWIFDkA/++xmT0XRMPt/QGFK2WEMsBseKNcgMlfmg8v3Kw
+y8RasQpTKstcDMLXLUKNOugppuHpVxMAwINqdrBhtqhOPYIisIk77JtAVQPTmPZn
+OGJRuSWeZQgOTJlCkXxhAlM/soMIn9ctVQU+blYT0igWoC++H/Ttd+CpHqa5B6kS
+NXpvewrgy8CDFHtE2hP2fXHeSLups5O9hPBdWMrouVwIYrZjiOQ//Hssfh4z01WT
+9m1kUngf0QPezE0V/uZoiA==
 
 `pragma protect key_keyowner="Mentor Graphics Corporation"
 `pragma protect key_keyname="MGC-VERIF-SIM-RSA-1"
 `pragma protect key_method="rsa"
 `pragma protect encoding=(enctype="base64", line_length=64, bytes=128)
 `pragma protect key_block
-cvc91L1dz2ahyKui1gFB7ztaBJlPCrTUGJvXiYcOPaCk67v+CT30gb82/knFhYg7
-y65hrpM+NdZHw5yKoKX/cJwMxtwHz7vAfZ+0ZemczjXVmq51Uwc3YhIyOQCuk0vF
-TFfcZ4jOcx1bnYJiYGfrOIbGaffhpsOX/Ndk/N6NqG8=
+kNrgrg5v0RKamzIc/WHG1r4YGFIOsG/0BZK6bOXqTkysQaLbyhZ8soyQt73V6lO/
+4kTfZYefEN3nGhAKsIMEAuLZaNY1Mm7fuPDJ5V9kg0dXzRAQsf5ZxIIt1OWZSRj2
+v99GM7M0jheE4lx/hIOFzdtyDFw3Spl3uetHgqeVXKg=
 
 
 `pragma protect data_method="aes256-cbc"
-`pragma protect encoding=(enctype="base64", line_length=64, bytes=1171)
+`pragma protect encoding=(enctype="base64", line_length=64, bytes=837)
 `pragma protect data_block
-qTxXWpl7LLlTvT5+HSqaD3b0tKAjMR5or3U5TMIG1SlX4HkysSgxRB3NkyobWI/P
-2XBEHk2yb1kH8VcdPTsJOz4e3/eR+eEXde40JuKIlMAGo0X7Eer58eH9kTws+cSW
-GQGGo507DOElR1kDL1Et1peg7fvnWCMhCZU4IFaIvHJizsg+TvRRRdgiEKSOTzpf
-MZRYKw32EBr0YYuLelRKixrTFRswWAaIxAaImWyH/r19TRi+5KDC5RLmSxiWlZlM
-mA9AM0AGxg0E9e7CEASusTgGDtZx/qzJNvvLp6ls/4YTfZ52Kdmoc70cq/OOEpMr
-mgdGdRiEyjw0BXWE2px/JGH+1TjdJFrvqtexVfLjl39QuJw/gKXbdUfYx9Z4nd7k
-xsvIxS+rOgON4G+VNiYqBNywzGOe5tOD/3rPYx+6UOQQIPG0jxkcr6dyQXpTUuz+
-OwCRtcOiPTX/gm5geTkwKfcyrIL7ijZtfqBr5dADEUPT4R42WAJvblsI9tmS/ioz
-jLAg9VUrgBRQ08qZy2y148NGaD9w4S1gu6xvEMiJdd+yJf3R/xayFKXVcQeEzjAW
-/zCrpPNhUgfj7YshPGR2FQlgjsv+jAiOA56Ocq2bbjK2OvZ1DytWKz3Gjktm7ZCi
-mCFH2Da+WJhtB57mL9qROkJNbEeLc+RkgdQHwuHkwoPIsZuZctrA4JOyW7Oh0OSA
-aFPRu24bmWO0mmvzmCFWtKAkGFDKafJbkOL2Z9CEPrITUdUq4Sxo+ioCnJJo/fwQ
-xk1nECNuwzYjg50KKTKH8BB/NyEJzfQb/rq7FB4tCurWajgp4QWpFV/c7jtVfP5o
-sdtN0FLdFpcSotjMJAPDn7oo4Og6RqsYU4T4uW8l/MvAV01RtAGkeUyIyAyE80vS
-0c5Heoi1gEclhxST20CqVtpGO3EP2aJ9pY7I6d78/CZ98u/RrDeRJEgOAubDybbK
-EAkFK4JOLrxwjoQEolOmcdeT47dE0ADdgp5xGhZOZncnrcb/u0VzdEE2tyyB1Qm+
-ZjzBQRELBNGd+uNviDLfKpIcNym3nJaqVGUa41cnW53tMmhvaUIh6yE212fCCPXU
-RCtTbqLp+870GjIN/1zCyVMBaNrgap0GnKxR+4r8IhJxDbOgk34bZ6x9cvTq2RE0
-vge1YJuuaNPsd4NdOeoY+pkz+o1rzklqJmg01AjVr9LKbFsk4eCpUbut+SDXKXD/
-3+vjrhItgN91Y6gQS1QqlF9XOnBW8Xd9VwFUTwuDMgDgCijEClBPVSVf9eqGELpu
-PSWOkwF9oYvH5P0W/r7Sd7EkVX9jfSnUh5GkARsRCPkp3/EckmJrHzzXSF8pMQvL
-QZDeffZ84BhPCSuL7QchA5KNALt+IbKFgESUV77MIv8neYr6OUbDl15laWSeOrmp
-VuyFYKlYQTEDs5yJ+3+zFXsIuwzHV/R7zJnA/iYJ3eJsZbOF5KDk5Mbafwr60uXf
-2M7JHLWz5NAGmVp+NrO5OFGkTstmP780GO84EfFmWrEnZUI79UuXNgui35FKXmDw
-lJsk1Pv/5El22sfbwHgKFhYXcbH9K1Px7xfRBI9w3ho=
+qBy3TC0n9I+80/XOLcjNwvTBou8UiefIFqp6xse8TYqzpGp19RgK0PfTuUKYTp+1
+DAFCuS0AHOO6W+T7lOgOvUQZ3XTtZEfRG9sq+kYm9SdbR2OlRIoEtjP2lzfHjOop
+FGjUR7lQMY9FaXeQPtWJR97qQ1ngB4mouNPxCZqCSry6ar/2lg3RiXWRZB5KPwY0
+rb4uDZ3cUmNZBAYhxOBR1+LAb8ZVQaHMpO/8dgytBo+l2RqYsNBjdqj7oUAybIv/
+Wj5Gd5Z7g7yzYchy41kNqVtrYqZFFBTj2upKp34NYqfZsBMHt1iDxOk3JefMLGu+
+8US55cCyyvIZhUS80rBBd9zuiHGt+IxtiTPEhFXbhb+rTbI1CDC48cfQZ60r619M
+gDX7QPirwbYcoXc7/zmJYfVQh66knHNaZ+4kwdXgTivirx50DSu9OStivye8PU8w
+DCqF8ybEs05R2rnTgop1io1HU9K7NDFiNWyeee9Niyze/j+OgE/3R8u58alUganU
+V/6NPngJP1aW4ntm//S81XENlcNdLglesPMaJMEEE+idLZaKX5iBfC2Dsrlo8/yr
+3zUYyQkhcCZZL9zvtfoRcx6mJD1Itdgz3oLqdCZ3ilZCGuFgC6zWf44vAqqw3UeJ
+dbbZzlvjFOvsREC/J8aVMvTULgDJMkHATJDfMa4RgZw8JVIn6DIVIKDR4eiAK6c7
+LXT9MZOOfaW5sNngLFQUb61HQf6hcmyY2reStM119amSX42niljV2alVwMLfZ6gB
+LmbSzC1F5OIryjVHbRMzd+Y2TZuPncWmIb57ZPJ9J3xdhPa6vhkJvtXn9FTr2TRB
+DVbtFf1PblAjAHtu2b9Niyj96WRzw7+jAEgla9Ct0xNmb/nlcGAdAHyyTMls2cLT
+32eZFdasu7J07UgklTas8qW4Ut5/KwZyr4L0AydiPUhkTwrvNfyJLuBKB3S+MyCB
+019y/3HgDXyRrUaD5U5aHubTSr46dmGojw6M45q9f8eICvr3ZiV2Aan2uRxBM33Y
+nXqh8ZFZS5dqKjQVmjMiTxQKbUsbeFKmFoAtnxrMXk2kxF3iNPMITtV72rV9i320
+qimnVae7I+hbftEZO7GKCfPMZZXTu4uIsj6GOuZFIRk=
 `pragma protect end_protected
 
 endmodule
